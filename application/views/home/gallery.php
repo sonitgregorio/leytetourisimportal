75079be0b28663d1ee1bf3562949cb1aa2bfe7c4
sonitgregorio <sonitgregorio@gmail.com>
2015-08-16 04:47:20 -0700

    Added images/upload page

--- application/views/home/gallery.php
+++ application/views/home/gallery.php
@@ -1,7 +1,32 @@
-<div class="col-md-10 col-md-offset-1">
+<div class="col-md-12 ">
   <div class="panel logins p-body" style="margin-top:0;box-shadow:none;margin-top:60px">
     <div class="panel-heading grad" style="box-shadow:none"><h4 style="color:#FFFF00;"><span class="glyphicon glyphicon-picture"></span>&nbsp;Gallery</h4></div>
       <div class="panel-body">
+
+            <div class="col-md-12" style="padding:0">
+
+                  <!-- Nav tabs -->
+                  <ul class="nav nav-tabs" role="tablist">
+                    <li role="presentation" class="active"><a href="#upload" aria-controls="home" role="tab" data-toggle="tab">Upload</a></li>
+                    <li role="presentation"><a href="#images" aria-controls="profile" role="tab" data-toggle="tab">Images</a></li>
+                  </ul>
+
+                  <!-- Tab panes -->
+                  <div class="tab-content">
+                    <div role="tabpanel col-md-12" class="tab-pane active" id="upload"  style="background-color:#FFFFE0;">
+                        <br />
+                        <?php $this->load->view('home/upload') ?>
+                    </div>
+                    <div role="tabpanel col-md-12" class="tab-pane" id="images" style="background-color:#FFFFE0;">
+                      <br />
+                      <div class="col-md-12" style="background-color: #FFFFE0" >
+                             <?php $this->load->view('home/images') ?>
+                      </div>
+                    </div>
+                </div>
+              </div>
+
+
 
       </div>
   </div>
